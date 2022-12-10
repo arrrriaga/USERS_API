41356061e0e1220797affc200b50f651ad17458a
Francisco Arriaga Velasco <94820722+arrrriaga@users.noreply.github.com>
2022-12-10 02:04:12 -0600

    VerInfoUsuario

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -142,6 +142,28 @@ const actualizarUsuario = async (req, res) => {
   }
 };
 
+const verInfoUsuario = async (req, res) => {
+  try {
+    const usuarioInfo = await User.findById(req.user.idUser, {
+      nombre: 1,
+      correo: 1,
+      tipo: 1,
+      edad: 1,
+      img: 1,
+      apellido: 1,
+    });
+    if (!usuarioInfo)
+      return res
+        .status(404)
+        .json({ mensaje: "Error", detalles: "Este usuario no existe." });
+    return res
+      .status(200)
+      .json({ mensaje: "Usuarios encontrados", detalles: usuarioInfo });
+  } catch (e) {
+    return res.status(400).json({ mensaje: "Error", detalles: e.message });
+  }
+};
+
 module.exports = {
   registro,
   login,
@@ -150,6 +172,7 @@ module.exports = {
   eliminarUsuario,
   eliminarUsuariosPorFiltro,
   actualizarUsuario,
+  verInfoUsuario,
   //verUsuarios,
   //eliminarUsuario
 };
